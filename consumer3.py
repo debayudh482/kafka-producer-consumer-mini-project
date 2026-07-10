fa96265832c0fb17c41e411aba0205515744de05
debayudh482 <debayudh2021@gmail.com>
2026-07-10 21:01:03 +0530

    Update consumer3.py

--- consumer3.py
+++ consumer3.py
@@ -10,19 +10,19 @@ from datetime import datetime
 
 # Define Kafka configuration
 kafka_config = {
-    'bootstrap.servers': 'pkc-619z3.us-east1.gcp.confluent.cloud:9092',
+    'bootstrap.servers': 'BOOTSTRAP SERVER',
     'sasl.mechanisms': 'PLAIN',
     'security.protocol': 'SASL_SSL',
-    'sasl.username': 'GOLQZTZY2L3FYG4X',
-    'sasl.password': 'cfltzrcmjxXol9ayf6xpl1aFPEVfplW24y1TXWHcb9XbkbexgcoEgwn4pbTSRaNQ',
+    'sasl.username': 'API key',
+    'sasl.password': 'API secret',
     'group.id': 'group31',
     'auto.offset.reset': 'latest'  # Start reading from the latest message if no offset is committed
 }
 
 # Create a Schema Registry client
 schema_registry_client = SchemaRegistryClient({
-  'url': 'https://psrc-1o8pr0d.us-east1.gcp.confluent.cloud',
-  'basic.auth.user.info': '{}:{}'.format('EALUUCGFDPS2A5EH', 'cfltWGWAhHB5ks2ixHuwfV+n+Igq0sRrbP9IhQwniFyKourt7+9jaNkrUeq5g1aQ')
+  'url': 'EENDPOINT',
+  'basic.auth.user.info': '{}:{}'.format('API key', 'API secret')
 })
 
 # Fetch the latest Avro schema for the value
