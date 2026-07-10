1b343f0ade5fd077c4e994a587d059cc7132b0df
debayudh482 <debayudh2021@gmail.com>
2026-07-10 20:53:18 +0530

    Add files via upload

---
+++ consumer3.py
@@ -0,0 +1,108 @@
+import threading
+from confluent_kafka import DeserializingConsumer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroDeserializer
+from confluent_kafka.serialization import StringDeserializer
+import json
+import os
+from datetime import datetime
+
+
+# Define Kafka configuration
+kafka_config = {
+    'bootstrap.servers': 'pkc-619z3.us-east1.gcp.confluent.cloud:9092',
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': 'GOLQZTZY2L3FYG4X',
+    'sasl.password': 'cfltzrcmjxXol9ayf6xpl1aFPEVfplW24y1TXWHcb9XbkbexgcoEgwn4pbTSRaNQ',
+    'group.id': 'group31',
+    'auto.offset.reset': 'latest'  # Start reading from the latest message if no offset is committed
+}
+
+# Create a Schema Registry client
+schema_registry_client = SchemaRegistryClient({
+  'url': 'https://psrc-1o8pr0d.us-east1.gcp.confluent.cloud',
+  'basic.auth.user.info': '{}:{}'.format('EALUUCGFDPS2A5EH', 'cfltWGWAhHB5ks2ixHuwfV+n+Igq0sRrbP9IhQwniFyKourt7+9jaNkrUeq5g1aQ')
+})
+
+# Fetch the latest Avro schema for the value
+subject_name = 'product_data'
+
+schema_str = schema_registry_client.get_latest_version(subject_name).schema.schema_str
+
+# Create Avro Deserializer for the value
+key_deserializer = StringDeserializer('utf_8')
+avro_deserializer = AvroDeserializer(schema_registry_client, schema_str)
+
+# Define the DeserializingConsumer
+consumer = DeserializingConsumer({
+    'bootstrap.servers': kafka_config['bootstrap.servers'],
+    'security.protocol': kafka_config['security.protocol'],
+    'sasl.mechanisms': kafka_config['sasl.mechanisms'],
+    'sasl.username': kafka_config['sasl.username'],
+    'sasl.password': kafka_config['sasl.password'],
+    'key.deserializer': key_deserializer,
+    'value.deserializer': avro_deserializer,
+    'group.id': kafka_config['group.id'],
+    'auto.offset.reset': kafka_config['auto.offset.reset'],
+    #'enable.auto.commit': True,
+    #'auto.commit.interval.ms': 5000 # Commit every 5000 ms, i.e., every 5 seconds
+})
+
+# To handle serialization of datetime objects,defining a custom encoder.
+def datetime_encoder(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+# Path to the separate JSON file for each consumer
+file_path = 'consumer3.json'  
+
+# Python function to load append the json string data into json file.
+def write_to_json_file(json_string, file_path):
+    with open(file_path, 'a') as file:
+        file.write(json_string + '\n')
+
+# Subscribe to the 'retail_data' topic
+consumer.subscribe(['product_updates'])
+
+# Continually read messages from Kafka
+try:
+    while True:
+        msg = consumer.poll(1.0)
+
+        if msg is None:
+            continue
+        if msg.error():
+            print('Consumer error: {}'.format(msg.error()))
+            continue
+        
+        #Change the category column to lowercase,in source it's in uppercase.
+        msg.value()['category'] = msg.value()['category'].lower() 
+        
+        # updating the price to half if product belongs to 'category a'
+        if msg.value()['category'] == 'category a':
+            
+            msg.value()['price'] = msg.value()['price'] * 0.5
+            msg.value()['price'] = round(msg.value()['price'],2)
+            
+        print('Successfully consumed record with key {} and value {}'.format(msg.key(), msg.value()))
+        json_string = json.dumps(msg.value(), default=datetime_encoder)
+
+        def write_to_json_file(json_string, file_path):
+            with open(file_path, 'a') as file:
+                file.write(json_string + '\n')
+
+        # Check if the file exists
+        if not os.path.isfile(file_path):
+            # Create the file and write the initial data
+            with open(file_path, 'w') as file:
+                file.write(json_string + '\n')
+        else:
+            # Append the data to the existing file
+            write_to_json_file(json_string, file_path)
+            print("json_string data is added to the JSON file.")
+        
+           
+except KeyboardInterrupt:
+    pass
+finally:
+    consumer.close()
---
+++ produce.py
@@ -0,0 +1,124 @@
+import mysql.connector
+import json
+from decimal import *
+
+from confluent_kafka import SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import StringSerializer
+
+
+def delivery_report(err, msg):
+    """
+    Reports the failure or success of a message delivery.
+
+    Args:
+        err (KafkaError): The error that occurred on None on success.
+        msg (Message): The message that was produced or failed.
+
+    Note:
+        In the delivery report callback the Message.key() and Message.value()
+        will be the binary format as encoded by any configured Serializers and
+        not the same object that was passed to produce().
+        If you wish to pass the original object(s) for key and value to delivery
+        report callback we recommend a bound callback or lambda where you pass
+        the objects along.
+    """
+    if err is not None:
+        print("Delivery failed for record {}: {}".format(msg.key(), err))
+        return
+    print('Record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(), msg.topic(), msg.partition(), msg.offset()))
+    
+
+kafka_config = {
+    'bootstrap.servers': 'pkc-619z3.us-east1.gcp.confluent.cloud:9092',
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': 'GOLQZTZY2L3FYG4X',
+    'sasl.password': 'cfltzrcmjxXol9ayf6xpl1aFPEVfplW24y1TXWHcb9XbkbexgcoEgwn4pbTSRaNQ'
+}
+
+schema_registry_client = SchemaRegistryClient({'url': 'https://psrc-1o8pr0d.us-east1.gcp.confluent.cloud',
+                                             'basic.auth.user.info': '{}:{}'.format('EALUUCGFDPS2A5EH', 'cfltWGWAhHB5ks2ixHuwfV+n+Igq0sRrbP9IhQwniFyKourt7+9jaNkrUeq5g1aQ')})
+
+subject_name = 'product_data'
+schema_str = schema_registry_client.get_latest_version(subject_name).schema.schema_str
+
+key_serializer = StringSerializer('utf_8')  
+avro_serializer = AvroSerializer(schema_registry_client, schema_str)
+
+producer = SerializingProducer({
+    'bootstrap.servers': kafka_config['bootstrap.servers'],
+    'security.protocol': kafka_config['security.protocol'],
+    'sasl.mechanisms': kafka_config['sasl.mechanisms'],
+    'sasl.username': kafka_config['sasl.username'],
+    'sasl.password': kafka_config['sasl.password'],
+    'key.serializer': key_serializer,  # Key will be serialized as a string
+    'value.serializer': avro_serializer  # Value will be serialized as Avro
+})
+
+
+conn=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="debayudh",
+    database="kafka_prac"
+)
+
+cursor=conn.cursor()
+config_data={}
+last_read_timestamp=None
+try:
+    with open("config.json") as f:
+        config_data=json.load(f)
+        last_read_timestamp=config_data.get("last_read_timestamp")
+except FileNotFoundError:
+    pass
+
+
+if last_read_timestamp is None:
+    last_read_timestamp="1970-01-01 00:00:00"
+    
+
+query="SELECT * FROM Product WHERE last_updated > '{}'".format(last_read_timestamp)
+cursor.execute(query)
+rows=cursor.fetchall()
+
+if not rows:
+    print("No rows to fetch.")
+else:
+    # Iterate over the cursor and produce to Kafka
+    for row in rows:
+        # Get the column names from the cursor description
+        columns = [column[0] for column in cursor.description]
+        # Create a dictionary from the row values
+        value = dict(zip(columns, row))
+        # Produce to Kafka
+        producer.produce(topic='product_updates', key=str(value['id']), value=value, on_delivery=delivery_report)
+        producer.flush()
+
+# Fetch any remaining rows to consume the result
+cursor.fetchall()
+
+query = "SELECT MAX(last_updated) FROM product"
+cursor.execute(query)
+
+# Fetch the result
+result = cursor.fetchone()
+max_date = result[0]  # Assuming the result is a single value
+
+# Convert datetime object to string representation
+max_date_str = max_date.strftime("%Y-%m-%d %H:%M:%S")
+
+# Update the value in the config.json file
+config_data['last_read_timestamp'] = max_date_str
+
+with open('config.json', 'w') as file:
+    json.dump(config_data, file)
+
+# Close the cursor and database connection
+cursor.close()
+conn.close()
+
+print("Data successfully published to Kafka")
